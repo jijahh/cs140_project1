cbce9e6449cb507655d97f3287d8a882ebec114f
mycronare <ardelavega@up.edu.ph>
2024-12-20 20:36:03 +0800

    edits

--- mlfq.py
+++ mlfq.py
@@ -37,7 +37,6 @@ class Queue:
         self.ready_queue = []
         self.running_process = None
 
-
 class Queue_1(Queue):
     def __init__(self, time_allotment):
         super().__init__("queue_1")
@@ -59,11 +58,11 @@ class Queue_1(Queue):
             else:
                 self.running_process.is_done = True
                 self.running_process.finish_time = mlfq.time
-                mlfq.to_print[1].append(process.name)
-                mlfq.done_processes.append(running_process)
+                mlfq.to_print[1].append(self.running_process.name)
+                mlfq.done_processes.append(self.running_process)
             self.ready_queue.remove(self.running_process)
             self.running_process = None
-        if self.running_process.time_allotment == 0:
+        if self.running_process is not None and self.running_process.time_allotment == 0:
             mlfq.to_print[5].append(self.running_process.name)
             self.running_process.queue += 1
             mlfq.queue_list[self.running_process.queue].enqueue(self.running_process)
@@ -116,14 +115,17 @@ class Queue_3(Queue):
     def __init__(self):
         super().__init__("queue_3")
         self.enqueue_list = []
-
+        
     def run(self, mlfq):
+        print(self.ready_queue)
+
         if self.running_process is None:
             self.running_process = self.ready_queue[0]
+
         self.running_process.burst_times[0] -= 1
         if self.running_process.burst_times[0] == 0:
             del self.running_process.burst_times[0]
-            if not self.running_process.check_io():
+            if not self.running_process.check_io(): 
                 mlfq.in_io.append(self.running_process)
             else:
                 self.running_process.is_done = True
@@ -140,7 +142,8 @@ class Queue_3(Queue):
         self.enqueue_list = sorted(
             self.enqueue_list, key=lambda x: sum(x.burst_times) + sum(x.io_times)
         )
-        self.ready_queue.append(self.enqueue_list)
+        if self.enqueue_list != []:
+            self.ready_queue.append(self.enqueue_list)
 
 
 class MLFQ:
@@ -168,16 +171,16 @@ class MLFQ:
         self.is_context_switching = False
         self.to_print = [[], [], [[], [], []], [], [], []]
 
+
     def run(self):
         while True:
             self.queue_list[2].enqueue_everything()
+            self.not_arrived = list(filter(lambda x: x.has_arrived == False, self.not_arrived))
             for process in self.not_arrived:
                 if process.arrival_time == self.time:
                     self.to_print[0].append(process.name)
                     process.has_arrived = True
                     self.queue_list[0].enqueue(process)
-                    process.has_arrived = True
-                    self.not_arrived.remove(process)
             self.active_queue.run(self)
             for process in self.in_io:
                 if process.io_times[process.io_index] == 0:
@@ -193,17 +196,19 @@ class MLFQ:
                 else:
                     process.io_times[0] -= 1
             for i in range(len(self.queue_list)):
-                self.to_print[2][i].append(
-                    [j.name for j in self.queue_list[i].ready_queue]
-                )
-            self.to_print[3].append(self.active_queue.ready_queue[0])
+                for j in self.queue_list[i].ready_queue:
+                    if j != []:
+                        self.to_print[2][i].append(j.name)
+            if self.active_queue.ready_queue:
+                self.to_print[3].append(self.active_queue.ready_queue[0].name)
             for i in self.queue_list:
                 if i.ready_queue:
                     self.active_queue = i
+                    break
             for process in self.in_io:
                 to_print[4].append(process.name)
             self.print_everything()
-            if check_done():
+            if self.check_done():
                 self.done()
                 break
             self.time += 1
@@ -224,14 +229,16 @@ class MLFQ:
         print(f"At time {self.time}")
         if self.to_print[0]:
             print("Arriving :", end=" ")
+            print(self.to_print[0])
         for i in self.to_print[1]:
             print(f"{i} DONE")
         print("Queues :", end=" ")
-        print(to_print[2])
-        print(f"CPU: {to_print[3]}")
-        print(f"I/O: {to_print[4]}")
+        print(self.to_print[2])
+        print(f"CPU: {self.to_print[3]}")
+        print(f"I/O: {self.to_print[4]}")
         for i in self.to_print[5]:
             print(f"{i} DEMOTED")
+        self.to_print = [[], [], [[], [], []], [], [], []]
 
     def done(self):
         print("SIMULATION DONE")
@@ -268,6 +275,7 @@ for i in range(num_processes):
         else:
             io_times.append(int(process_deets[i]))
     processes.append(Process(process_name, burst_times, io_times, arrival_time))
+
 
 mlfq = MLFQ(
     num_processes,
