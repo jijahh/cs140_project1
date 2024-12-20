7a2ddf6d71cb1f93b6ab7ea1001ea4612826b59b
jijahh <eagalang@up.edu.ph>
2024-12-20 23:29:01 +0800

    AASD

--- mlfq.py
+++ mlfq.py
@@ -37,6 +37,7 @@ class Queue:
         self.ready_queue = []
         self.running_process = None
 
+
 class Queue_1(Queue):
     def __init__(self, time_allotment):
         super().__init__("queue_1")
@@ -51,10 +52,12 @@ class Queue_1(Queue):
             return
         if self.running_process is None:
             self.running_process = self.ready_queue[0]
-        self.curr_time_quantum -= 1 
+        self.curr_time_quantum -= 1
         self.running_process.burst_times[0] -= 1
         self.running_process.time_allotment -= 1
         print(self.running_process.time_allotment)
+        print(self.curr_time_quantum)
+        print(self.running_process.burst_times[0])
         if self.running_process.burst_times[0] == 0:
             del self.running_process.burst_times[0]
             if not self.running_process.check_io():
@@ -66,13 +69,18 @@ class Queue_1(Queue):
                 mlfq.done_processes.append(self.running_process)
             self.ready_queue.remove(self.running_process)
             self.running_process = None
-        if self.running_process is not None and self.running_process.time_allotment == 0:
+            self.curr_time_quantum = self.time_quantum
+        elif (
+            self.running_process is not None
+            and self.running_process.time_allotment == 0
+        ):
             mlfq.to_print[5].append(self.running_process.name)
             self.running_process.queue += 1
             mlfq.queue_list[self.running_process.queue].enqueue(self.running_process)
             self.ready_queue.remove(self.running_process)
             self.running_process = None
-        if self.curr_time_quantum == 0:
+            self.curr_time_quantum = self.time_quantum
+        elif self.curr_time_quantum == 0:
             self.ready_queue.append(self.running_process)
             del self.ready_queue[0]
             self.curr_time_quantum = self.time_quantum
@@ -122,7 +130,7 @@ class Queue_3(Queue):
     def __init__(self):
         super().__init__("queue_3")
         self.enqueue_list = []
-        
+
     def run(self, mlfq):
         # print(self.ready_queue)
 
@@ -132,7 +140,7 @@ class Queue_3(Queue):
         self.running_process.burst_times[0] -= 1
         if self.running_process.burst_times[0] == 0:
             del self.running_process.burst_times[0]
-            if not self.running_process.check_io(): 
+            if not self.running_process.check_io():
                 mlfq.in_io.append(self.running_process)
             else:
                 self.running_process.is_done = True
@@ -152,6 +160,7 @@ class Queue_3(Queue):
         if self.enqueue_list != []:
             self.ready_queue = self.ready_queue + self.enqueue_list
         self.enqueue_list = []
+
 
 class MLFQ:
     def __init__(
@@ -178,19 +187,27 @@ class MLFQ:
         self.is_context_switching = False
         self.to_print = [[], [], [[], [], []], [], [], []]
 
-
     def run(self):
         while True:
-            self.not_arrived = list(filter(lambda x: x.has_arrived == False, self.not_arrived))
+            self.not_arrived = list(
+                filter(lambda x: x.has_arrived == False, self.not_arrived)
+            )
             for process in self.not_arrived:
                 if process.arrival_time == self.time:
                     self.to_print[0].append(process.name)
                     process.has_arrived = True
                     self.queue_list[0].enqueue(process)
-            # print(self.active_queue.name)
+            for i in self.queue_list:
+                if i.ready_queue:
+                    self.active_queue = i
+                    break
             self.active_queue.run(self)
+            for i in self.queue_list:
+                if i.ready_queue:
+                    self.active_queue = i
+                    break
             for process in self.in_io:
-                if process.io_times[0]== 0:
+                if process.io_times[0] == 0:
                     del process.io_times[0]
                     if process.check_done():
                         self.done_processes.append(process)
@@ -204,17 +221,17 @@ class MLFQ:
                     process.io_times[0] -= 1
             self.queue_list[2].enqueue_everything()
             for i in range(len(self.queue_list)):
-                for j in self.queue_list[i].ready_queue:
-                    if j != []:
+                if self.queue_list[i].ready_queue:
+                    for j in self.queue_list[i].ready_queue:
                         self.to_print[2][i].append(j.name)
             if self.active_queue.ready_queue:
                 self.to_print[3].append(self.active_queue.ready_queue[0].name)
-            for i in self.queue_list:
-                if i.ready_queue:
-                    self.active_queue = i
-                    break
             for process in self.in_io:
                 self.to_print[4].append(process.name)
+            for i in self.queue_list:
+                if i.ready_queue:
+                    for j in i.ready_queue:
+                        j.wait_time += 1
             self.print_everything()
             if self.check_done():
                 self.done()
@@ -241,7 +258,12 @@ class MLFQ:
         for i in self.to_print[1]:
             print(f"{i} DONE")
         print("Queues :", end=" ")
-        print([x[1:] if x else [] for x in self.to_print[2] ])
+        print(
+            [
+                self.to_print[2][x][1:] if x == 0 else self.to_print[2][x]
+                for x in range(len(self.to_print[2]))
+            ]
+        )
         print(f"CPU: {self.to_print[3]}")
         print(f"I/O: {self.to_print[4]}")
         for i in self.to_print[5]:
@@ -256,11 +278,13 @@ class MLFQ:
             turnaround_time = i.finish_time - i.arrival_time
             turnaround_times.append(turnaround_time)
             print(
-                f"Turnaround time for Process {i.name}: {i.finish_time} - {i.arrival_time} = {turnaround_time}"
+                f"Turnaround time for Process {i.name}: {i.finish_time} - {i.arrival_time} = {turnaround_time} ms"
             )
         print(
-            f"Average Turnaround Time = {sum(turnaround_times)/len(turnaround_times)}"
+            f"Average Turnaround Time = {sum(turnaround_times)/len(turnaround_times)} ms"
         )
+        for i in self.done_processes:
+            print(f"Waiting time for Process {i.name}: {i.wait_time} ms")
 
 
 print("# Enter Scheduler Details #")
